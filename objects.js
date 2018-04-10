2db7c0e15add83b84285a08b4f777d0706144e69
Nicholas Powers <njpowers7915@gmail.com>
2018-04-10 14:53:04 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+const aratistName = 'Tiesto'
+var playlist = {[artistName]: 'Paradise'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign(playlist, {[artistName]: songTitle});
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
